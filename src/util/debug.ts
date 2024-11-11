1f1436ad779edbed3a2d14bc9d2cac24e62f2970
Michiel de Jong <michiel@pondersource.com>
2024-11-11 14:57:27 +0100

    It works

--- src/util/debug.ts
+++ src/util/debug.ts
@@ -1,5 +1,5 @@
 import { promises } from 'node:fs';
 
 export async function debug(str: string): Promise<void> {
-  await promises.appendFile('debug.txt', `${str}\n`);
+  // await promises.appendFile('debug.txt', `${str}\n`);
 }
