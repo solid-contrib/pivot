9a6f30a1d89d72c5f5e00433bf7a3e37a6a34bbb
Michiel de Jong <michiel@pondersource.com>
2024-11-12 15:25:49 +0100

    Use PATCH code from NSS

--- src/storage/RdfPatchingStore.ts
+++ src/storage/RdfPatchingStore.ts
@@ -10,6 +10,14 @@ import {
   readableToString,
   BasicRepresentation
 } from '@solid/community-server';
+import { graph, parse, serialize } from 'rdflib';
+import { parsePatchDocument } from './patch/n3-patch-parser';
+
+
+// Patch parsers by request body content type
+const PATCH_PARSERS = {
+  'text/n3': parsePatchDocument
+};
 
 /**
  * {@link ResourceStore} using decorator pattern for the `modifyResource` function.
@@ -29,9 +37,29 @@ export class RdfPatchingStore<T extends ResourceStore = ResourceStore> extends P
     patch: Patch,
     conditions?: Conditions,
   ): Promise<ChangeMap> {
+    const resourceUrl = identifier.toString();
+    const resourceSym = (graph as any).sym(resourceUrl);
+    const resourceContentType = TEXT_TURTLE;
     let representation = await this.source.getRepresentation(identifier, { type: { [TEXT_TURTLE]: 1 }});
     const turtle: string = await readableToString(representation.data);
-    representation = new BasicRepresentation(turtle + '\n<oh> <hi> <there> .\n', identifier, TEXT_TURTLE);
+    const store = graph();
+    parse(turtle, store, resourceUrl, resourceContentType);
+    const parsePatch = PATCH_PARSERS['text/n3'];
+    const patchObject = await parsePatch(resourceUrl, resourceUrl, '');
+    await (graph as any).applyPatch(patchObject, resourceSym);
+    let serialized: string | undefined = await new Promise((resolve, reject) => {
+      serialize(resourceSym, graph as any, resourceUrl, resourceContentType, (err: Error | null | undefined, result: string | undefined): void => {
+        if (err !== null) {
+          reject(err);
+        }
+        resolve(result);
+      });
+    });
+    if (typeof serialized !== 'string') {
+      console.log('something went wrong');
+      serialized = turtle;
+    }
+    representation = new BasicRepresentation(serialized, representation.metadata, TEXT_TURTLE);
 
     return this.source.setRepresentation(identifier, representation);
   }
