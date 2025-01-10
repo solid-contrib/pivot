b836c0e63ff5a3424cf4bba6a5dc0827c4a37ea1
Michiel de Jong <michiel@pondersource.com>
2025-01-10 09:35:45 +0100

    Fork the OidcHttpHandler

---
+++ src/identity/PivotOidcHttpHandler.ts
@@ -0,0 +1,10 @@
+import { OidcHttpHandler, ProviderFactory } from '@solid/community-server';
+
+/**
+ * HTTP handler that redirects all requests to the OIDC library.
+ */
+export class PivotOidcHttpHandler extends OidcHttpHandler {
+//   public constructor(providerFactory: ProviderFactory) {
+//     super(providerFactory);
+//   }
+}
