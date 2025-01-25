e08a76a40b904bd66779371223f370b912fb18cb
bourgeoa <alain.bourgeois10@gmail.com>
2025-01-26 00:01:09 +0100

    fix slash issue

--- src/http/output/PivotResponseWriter.ts
+++ src/http/output/PivotResponseWriter.ts
@@ -27,25 +27,24 @@ export class PivotResponseWriter extends BasicResponseWriter {
   }
   public async handle(input: { response: HttpResponse; result: ResponseDescription }): Promise<void> {
     try {
-        if (
-          (input.response.req.method === 'GET') &&
-          (typeof input.response.req.url === 'string') &&
-          ([401, 403, 404].indexOf(input.result.statusCode) !== -1) &&
-          (hasTrailingSlash(input.response.req.url) === false)) {
-          const target = await this.targetExtractor.handleSafe({ request: input.response.req as HttpRequest });
-          const withSlash = addTrailingSlash(target.path);
-          const exists = await this.store.hasResource({ path: withSlash });
-          // console.log('exists', withSlash, exists);
-          if (exists) {
-            // console.log('rewriting', input.response.req.method, input.response.req.url, input.result.statusCode);
-            input.response.statusCode = 301;
-            input.response.setHeader('Location', withSlash);
-            input.response.end('Try adding a slash at the end of the URL.\n');
-            return;
-          }
+      if (
+        (input.response.req.method === 'GET') &&
+        (typeof input.response.req.url === 'string') &&
+        (input.result.statusCode === 404) &&
+        (hasTrailingSlash(input.response.req.url) === false)) {
+        const target = await this.targetExtractor.handleSafe({ request: input.response.req as HttpRequest });
+        const withSlash = addTrailingSlash(target.path);
+        const exists = await this.store.hasResource({ path: withSlash });
+        // console.log('exists', withSlash, exists);
+        if (exists) {
+          // console.log('rewriting', input.response.req.method, input.response.req.url, input.result.statusCode);
+          input.response.statusCode = 301;
+          input.response.setHeader('Location', withSlash);
+          input.response.end('Try adding a slash at the end of the URL.\n');
         }
+      }
     } catch (e) {
-        console.error(e);
+        /* path withSlash do not exist */
     }
     return super.handle(input);
   }
