ca8cc1dbb9d67466acb936f58014f5c9b6e56dd6
Michiel de Jong <michiel@pondersource.com>
2025-01-29 12:44:19 +0100

    Adopt https://github.com/solid-contrib/pivot/pull/81

--- src/http/output/PivotResponseWriter.ts
+++ src/http/output/PivotResponseWriter.ts
@@ -30,7 +30,9 @@ export class PivotResponseWriter extends BasicResponseWriter {
       if (
         (input.response.req.method === 'GET') &&
         (typeof input.response.req.url === 'string') &&
-        ([401, 403, 404].indexOf(input.result.statusCode) !== -1) &&
+        // This was changed in https://github.com/solid-contrib/pivot/pull/81:
+        // ([401, 403, 404].indexOf(input.result.statusCode) !== -1) &&
+        (input.result.statusCode === 404) &&
         (hasTrailingSlash(input.response.req.url) === false)) {
         const target = await this.targetExtractor.handleSafe({ request: input.response.req as HttpRequest });
         const withSlash = addTrailingSlash(target.path);
@@ -50,7 +52,7 @@ export class PivotResponseWriter extends BasicResponseWriter {
         }
       }
     } catch (e) {
-        console.error(e);
+      /* path withSlash do not exist */
     }
     return super.handle(input);
   }
