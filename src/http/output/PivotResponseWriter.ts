70a497c09857fdabc5a07de8279ffb5a23b1d1b8
Michiel de Jong <michiel@pondersource.com>
2025-01-29 12:33:26 +0100

    try-catch around exists check

--- src/http/output/PivotResponseWriter.ts
+++ src/http/output/PivotResponseWriter.ts
@@ -27,23 +27,28 @@ export class PivotResponseWriter extends BasicResponseWriter {
   }
   public async handle(input: { response: HttpResponse; result: ResponseDescription }): Promise<void> {
     try {
-        if (
-          (input.response.req.method === 'GET') &&
-          (typeof input.response.req.url === 'string') &&
-          ([401, 403, 404].indexOf(input.result.statusCode) !== -1) &&
-          (hasTrailingSlash(input.response.req.url) === false)) {
-          const target = await this.targetExtractor.handleSafe({ request: input.response.req as HttpRequest });
-          const withSlash = addTrailingSlash(target.path);
-          const exists = await this.store.hasResource({ path: withSlash });
-          // console.log('exists', withSlash, exists);
-          if (exists) {
-            // console.log('rewriting', input.response.req.method, input.response.req.url, input.result.statusCode);
-            input.response.statusCode = 301;
-            input.response.setHeader('Location', withSlash);
-            input.response.end('Try adding a slash at the end of the URL.\n');
-            return;
-          }
+      if (
+        (input.response.req.method === 'GET') &&
+        (typeof input.response.req.url === 'string') &&
+        ([401, 403, 404].indexOf(input.result.statusCode) !== -1) &&
+        (hasTrailingSlash(input.response.req.url) === false)) {
+        const target = await this.targetExtractor.handleSafe({ request: input.response.req as HttpRequest });
+        const withSlash = addTrailingSlash(target.path);
+        let exists = false;
+        try {
+           exists = await this.store.hasResource({ path: withSlash });
+        } catch (e) {
+           // leave as false
         }
+        // console.log('exists', withSlash, exists);
+        if (exists) {
+          // console.log('rewriting', input.response.req.method, input.response.req.url, input.result.statusCode);
+          input.response.statusCode = 301;
+          input.response.setHeader('Location', withSlash);
+          input.response.end('Try adding a slash at the end of the URL.\n');
+          return;
+        }
+      }
     } catch (e) {
         console.error(e);
     }
